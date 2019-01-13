679d6614d10907d85022858a083e6b9621d4ffe8
卢忠宽 <luzhongkuan@luzhongkuandeMacBook-Air.local>
2019-01-13 21:48:45 +0800

    fix

--- build/build.js
+++ build/build.js
@@ -6,7 +6,8 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const nodeExternals = require('webpack-node-externals')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const nodeExternals = require('webpack-node-externals')
 
 const baseConfig = require('./webpack.config')
 
@@ -15,16 +16,16 @@ const {
 } = require('./utils')
 
 const libName = 'kuan-wordcloud'
-const distPath = resolve('lib')
+const distPath = resolve('dist')
 
 // build config
 const webpackConfig = merge(baseConfig, {
-  entry: resolve('src'),
+  entry: resolve('src/demo.js'),
   output: {
     path: distPath,
-    filename: `${libName}.js`,
-    library: libName,
-    libraryTarget: 'commonjs2'
+    // filename: `${libName}.js`,
+    // library: libName,
+    // libraryTarget: 'commonjs2'
   },
   mode: 'production',
   optimization: {
@@ -33,7 +34,7 @@ const webpackConfig = merge(baseConfig, {
       new UglifyJsPlugin()
     ]
   },
-  externals: [nodeExternals()],
+  // externals: [nodeExternals()],
   plugins: [
     new MiniCssExtractPlugin({
       filename: `${libName}.css`,
@@ -41,12 +42,25 @@ const webpackConfig = merge(baseConfig, {
     new CleanWebpackPlugin([distPath], {
       root: process.cwd()
     }),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      filename: 'index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        minifyJS: true,
+        minifyCSS: true
+      },
+      chunksSortMode: 'dependency'
+    }),
   ]
 })
 
 // 开始编译
 webpack(webpackConfig, (err, stats) => {
-  const message = `${stats.toString({colors: true})} \n`
+  const message = `${stats.toString({ colors: true })} \n`
   if (err || stats.hasErrors()) {
     console.log(err || message)
     if (onFail) {
